18686e88b05c4137c91e062fd80b717d145342dd
Andy Barrett <apbarret@nsidc.org>
2021-10-19 18:58:15 -0600

    add some bare bones code

--- parse_ccsm3_sir_output.py
+++ parse_ccsm3_sir_output.py
@@ -1,2 +1,16 @@
 """Script to parse output files"""
 
+def main(filename):
+    """Does the job"""
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines[0:10]:
+        print(lines)
+
+    return
+
+
+if __name__ == '__main__':
+    filename = 'ccsm3_sir_de_output.dat'
+    main(filename)
