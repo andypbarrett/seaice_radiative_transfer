637ade616812a296e55833f3bc0586ee982ea391
Andy Barrett <apbarret@nsidc.org>
2021-10-20 09:17:16 -0600

    add parser for x = y statements

--- 1D_dE_CCSM/parse_ccsm3_sir_output.py
+++ 1D_dE_CCSM/parse_ccsm3_sir_output.py
@@ -1,12 +1,20 @@
 """Script to parse output files"""
 
+def parse_assignment(line):
+    """Parses a line of form x = y, and returns y"""
+    return float(line.split('=')[1].strip())
+
+
 def main(filename):
     """Does the job"""
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-    for line in lines[0:10]:
-        print(lines)
+#   Day of year line 6
+    print(parse_assignment(lines[5]))
+#   Latitude line 7
+#    for line in lines[0:10]:
+#        print(line)
 
     return
 
