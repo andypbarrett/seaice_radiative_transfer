cd2a43876e81194d97367dbc900e166061a088fd
Andy Barrett <apbarret@nsidc.org>
2022-02-28 18:31:34 -0700

    refactor to use for loop and add more experiments

--- 1D_dE_CCSM/run_ccsm3_1d_de_experiments.sh
+++ 1D_dE_CCSM/run_ccsm3_1d_de_experiments.sh
@@ -1,36 +1,37 @@
-!#/bin/bash
+#!/bin/bash
 
 input_file='ccsm3_sir_de_input.dat'
+output_file='ccsm3_sir_de_output.dat'
 output_dir='1D_DE_test'
 
-cp 1D_DE/ccsm3_sir_de_input_fig10bare.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10bare.dat"
+experiments="fig10bare\
+ fig10bare1sd\
+ fig10bare2sd\
+ fig10bareminus1sd\
+ fig10bareminus2sd\
+ fig10pond\
+ fig10pond1sd\
+ fig10pondminus1sd\
+ fig11bare15mminus1sd\
+ fig11bare15mminus2sd\
+ fig11bare15mplus1sd\
+ fig11bare15mplus2sd\
+ fig12mp1sd\
+ fig12mpminus1sd\
+ fig7bareice\
+ fig7ponded\
+ fig8bare15m\
+ fig8bare1m\
+ fig9pond"
 
-cp 1D_DE/ccsm3_sir_de_input_fig10bare1sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10bare1sd.dat"
+for experiment in $experiments
+do
+    exp_input_file="1D_DE/ccsm3_sir_de_input_$experiment.dat"
+    exp_output_file="$output_dir/ccsm3_sir_de_output_$experiment.dat"
+    echo "Running $exp_input_file -> $exp_output_file"
+    cp $exp_input_file $input_file
+    ./a.out
+    cp $output_file $exp_output_file
+done
 
-cp 1D_DE/ccsm3_sir_de_input_fig10bare2sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10bare2sd.dat"
-
-cp 1D_DE/ccsm3_sir_de_input_fig10bareminus1sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10bareminus1sd.dat"
-
-cp 1D_DE/ccsm3_sir_de_input_fig10bareminus2sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10bareminus2sd.dat"
-
-cp 1D_DE/ccsm3_sir_de_input_fig10pond.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10pond.dat"
-
-cp 1D_DE/ccsm3_sir_de_input_fig10pond1sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10pond1sd.dat"
-
-cp 1D_DE/ccsm3_sir_de_input_fig10pondminus1sd.dat $input_file
-./a.out
-cp ccsm3_sir_de_output.dat "$output_dir/ccsm3_sir_de_output_fig10pondminus1sd.dat"
+ls -ltr $output_dir
