98425d74e27e059e52099d6ec8e3dd48232d44e5
Andy Barrett <apbarret@nsidc.org>
2022-02-28 18:08:57 -0700

    refactor to move step plot to separate function, add tick_params

--- 1D_dE_CCSM/plot_fig10.py
+++ 1D_dE_CCSM/plot_fig10.py
@@ -1,12 +1,14 @@
 """Generates Figure 10 from Briegleib and Light page 90"""
 from pathlib import Path
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 
 from parse_ccsm3_sir_output import DeltaEdOutput
 
 
-spectral_bands = [(300., 700.), (700., 1200.), (1200., 2100.)]  # Need to fix
+spectral_bands = [(300., 700.), (700., 1190.), (1190., 2100.)]  # Need to fix
 
 OUTPUT_DIRPATH = Path.home() / 'src' / 'seaice_radiative_transfer' / '1D_dE_CCSM' / '1D_DE_test'
 FILE_PREFIX = 'ccsm3_sir_de_output'
@@ -24,40 +26,52 @@ EXPERIMENTS = ['bare1sd',
 linecolor = ['k', 'k', 'k', 'k', 'b', 'b']
 linestyle = ['--', '-.', '--', '-.', '--', '--']
 
+def plot_albedo_as_step(df, ax=None, label=None, color='k'):
+    """Helper function to make nice step plot"""
+    if not ax:
+        ax = plt.gca()
+
+    x = [300., 700., 1190., 5000.]
+    y = np.append(df['Total'].values, df['Total'][3])
+    ax.step(x, y, where='post', color=color,
+            linewidth=2, label=label)
+
+
 def main():
 
     fig, ax = plt.subplots(figsize=(10, 10))
     ax.set_xlim(300., 2100.)
     ax.set_ylim(0., 1.)
-    ax.set_ylabel('Albedo')
-    ax.set_xlabel('Wavelength (nm)')
+
+    ax.set_yticks(np.arange(0., 1.05, .1))
+    ax.set_yticks(np.arange(0.05, 1.05, .1), minor=True)
+
+    ax.set_xticks([500., 1000., 1500., 2000.])
+    ax.set_xticks(np.arange(300., 2150., 100.), minor=True)
+
+    ax.set_ylabel('Albedo', fontsize=20)
+    ax.set_xlabel('Wavelength (nm)', fontsize=20)
+
+    ax.tick_params(axis='both', which='major', labelsize=15, length=10)
+    ax.tick_params(which='minor', length=7)
 
     bare = DeltaEdOutput(OUTPUT_DIRPATH /
-                               f'{FILE_PREFIX}_fig{FIGURE}bare.dat')
-    salbedo = bare.spectral_albedos
-    salbedo.index = [300., 700., 1190.]
-    salbedo['Total'].plot(color='k', drawstyle='steps-post', linewidth=2,
-                           label='bare')
+                         f'{FILE_PREFIX}_fig{FIGURE}bare.dat')
+    plot_albedo_as_step(bare.spectral_albedos, ax=ax, color='k', label='Bare')
 
     pond = DeltaEdOutput(OUTPUT_DIRPATH /
                          f'{FILE_PREFIX}_fig{FIGURE}pond.dat')
-    salbedo = pond.spectral_albedos
-    salbedo.index = [300., 700., 1200.]
-    salbedo['Total'].plot(color='b', drawstyle='steps-post', linewidth=2,
-                           label='pond')
-    
+    plot_albedo_as_step(pond.spectral_albedos, ax=ax, color='b', label='Pond')
+
     for experiment, c, lst in zip(EXPERIMENTS, linecolor, linestyle):
         output = DeltaEdOutput(OUTPUT_DIRPATH /
                                f'{FILE_PREFIX}_fig{FIGURE}{experiment}.dat')
         salbedo = output.spectral_albedos
-        #salbedo.index = [300., 700., 1200.]
-        #salbedo['Direct'].plot(drawstyle='steps-post', linewidth=2,
-        #                       label=experiment)
         for index, row in salbedo.iterrows():
             ax.plot(spectral_bands[index-1], [row['Total'], row['Total']],
                     color=c, linestyle=lst)
 
-    ax.legend()
+    ax.legend(fontsize=20)
     plt.show()
 
 
