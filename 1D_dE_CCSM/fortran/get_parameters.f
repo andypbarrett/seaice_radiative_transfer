9d5c6d0f08e03f44f4c0a5c5fe9cbc4e4cf2df6e
Andy Barrett <apbarret@nsidc.org>
2022-05-26 15:41:38 -0600

    initial commit

---
+++ 1D_dE_CCSM/fortran/get_parameters.f
@@ -0,0 +1,13 @@
+C----------------------------------------------------------------------
+C     get_parameters: gets input parameters for ccsm2_sir_de sea ice
+C     radiative transfer model
+C
+C
+C----------------------------------------------------------------------
+      subroutine get_parameters()
+
+      implicit none
+
+
+      return
+      end
