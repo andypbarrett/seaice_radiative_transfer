8c85eff7da76e94731b1248d1e758c6a610886fa
Andy Barrett <apbarret@nsidc.org>
2022-07-25 16:34:29 -0600

    init commit of test function to return parameters

---
+++ 1D_dE_CCSM/return_parameters.f
@@ -0,0 +1,29 @@
+C----------------------------------------------------------------------
+C     Returns dummy parameters for testing
+C
+C
+C----------------------------------------------------------------------
+      subroutine return_parameters()
+
+      implicit none
+
+      integer
+     $     plon,
+     $     plev,
+     $     plevp,
+     $     plond,   ! slt extended domain longitude
+     $     nxpt    ! no.of points outside active domain for interpolant
+
+      parameter(plon = 1,
+     $          plev = 18,
+     $          nxpt = 1,
+     $          plevp = plev + 1,
+     $          plond = plon + 1 + 2*nxpt)
+      
+      real asdir(plond),        ! albedo: shortwave, direct
+     $     asdif(plond),        ! albedo: shortwave, diffuse
+     $     aldir(plond),        ! albedo: longwave, direct
+     $     aldif(plond)         ! albedo: longwave, diffuse
+
+      common /results/
+     $     asdir, asdif, aldir, aldif
