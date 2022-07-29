51d03cb9467369d9b278f8fa7abe8adfce82de93
Andy Barrett <apbarret@nsidc.org>
2022-07-29 13:47:51 -0600

    add func to set parameters in input_common

--- seaicert/ccsm3_sir_de.py
+++ seaicert/ccsm3_sir_de.py
@@ -1,6 +1,9 @@
 """Python wrapper for CCSM3_SIR_DE"""
+from dataclasses import dataclass
+
 import numpy as np
 
+from interface import input_common, output_common, seaice_common, test
 import default_input
 
 
@@ -51,6 +54,7 @@ class SeaIceRT():
 
     def run(self):
         """Run sea ice radiative transfer model"""
+        set_model_input(self.__dict__)
         return
 
     def __repr__(self):
@@ -62,4 +66,47 @@ class SeaIceRT():
             f"{self.latitude:5.2f}   latitude    (from +90 to -90)" + "\n" + \
             "\n"
         )
+
+
+def set_model_input(param_dict):
+    """Assigns model parameters to input_common structure
+
+    :param_dict: __dict__ object containing instance attributes of SeaIceRT
+    """
+    input_common.dayyr_in = param_dict["day_of_year"]
+    input_common.rlat_in = param_dict["latitude"]
+    input_common.ps_in = param_dict["surface_pressure"]
+    input_common.co2mix_in = param_dict["co2_volume_mixing_ratio"]
+    input_common.ts_in = param_dict["surface_air_temperature"]
+    input_common.tg_in = param_dict["ground_temperature"]
+    input_common.sndpth_in = param_dict["snow_depth"]
+    input_common.rhos_in = param_dict["snow_density"]
+    input_common.rs_in = param_dict["snow_grain_radius"]
+    input_common.hpnd_in = param_dict["pond_depth"]
+    input_common.R_pnd_in = param_dict["pond_tuning_parameter"]
+    input_common.hice_in = param_dict["sea_ice_thickness"]
+    input_common.R_ice_in = param_dict["sea_ice_tuning_parameter"]
+    
+    input_common.lev_in = to_array(param_dict["level"], np.int32)
+    input_common.pmidm1_in = to_array(param_dict["pressure"], np.float32)
+    input_common.tm1_in = to_array(param_dict["air_temperature"], np.float32)
+    input_common.qm1_in = to_array(param_dict["water_vapor_mixing_ratio"],
+                                   np.float32)
+    input_common.o3mmr_in = to_array(param_dict["ozone_mixing_ratio"], np.float32)
+    input_common.cld_in = to_array(param_dict["cloud_cover"], np.float32)
+    input_common.clwp_in = to_array(param_dict["cloud_liquid_water_path"],
+                                    np.float32)
+    return
+
    
+@dataclass
+class DEInput:
+    """
+    Input dataclass for Delta Eddington model
+
+    day_of_year: float  decimal day of year 1.0 to 365.99 1=January 1
+    latitude: float latitude of single column
+    """
+    day_of_year: float
+    latitude: float
+    
